1565a1ffd784ffe7617d35d4d951f91e44d9f5dd
UsfHajaj <yosefys236@gmail.com>
2025-05-13 08:38:01 +0300

    Add project files.

---
+++ Ecom.infrastructure/infrastructureRegisteration.cs
@@ -0,0 +1,31 @@
+﻿using Ecom.Core.Interfaces;
+using Ecom.infrastructure.Data;
+using Ecom.infrastructure.Repositries;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.infrastructure
+{
+    public static class infrastructureRegisteration
+    {
+        public static IServiceCollection infrastructureConfiguration(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddScoped(typeof(IGenericRepositry<>), typeof(GenericRepositry<>));
+            //Apply UnitOfWork
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddDbContext<AppDbContext>(op => 
+            {
+                op.UseSqlServer(configuration.GetConnectionString("EcomDatabase"));
+            }
+            );
+            return services;
+        }
+    }
+}
